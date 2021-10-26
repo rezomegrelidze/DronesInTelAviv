a48cc49c4aadeb6c4255ffec9e8b5e1017d54eda
Raz Megrelidze <rezomegrelidze@gmail.com>
2021-10-26 23:38:46 +0300

    commit done

--- DronesInTelAviv/app.js
+++ DronesInTelAviv/app.js
@@ -37,16 +37,58 @@ class AltitudeManager {
     }
 
     requestAltitude(drone) {
-        let availableAltitudes = this.availableAltitudes();
-        if (availableAltitudes.length > 0) {
-            drone.altitude = availableAltitudes[0];
-            return true;
-        }
-        return false;
+        const altitude = this.firstAvailableAltitude();
+        if (altitude == undefined) return false;
+        drone.altitude = altitude;
+        altitude.drone = drone;
+        return true;
     }
 
-    availableAltitudes() {
-        return this.controlSystem.altitudes.filter(a => !this.controlSystem.drones.some(d => d.altitude === a));
+    firstAvailableAltitude() {
+        return this.controlSystem.altitudes.find(a => a.drone === undefined);
+    }
+
+    removeAltitude(drone) {
+        if (drone.altitude == undefined) return;
+        drone.altitude = undefined;
+        const altitude = this.controlSystem.altitudes.find(a => a.drone && a.drone.id === drone.id);
+        altitude.drone = undefined;
+    }
+}
+
+class FlightManager {
+    constructor(controlSystem) {
+        this.controlSystem = controlSystem;
+    }
+
+    setNextFlightState(drone) {
+        let positionBackup = { x: drone.position.x, y: drone.position.y };
+
+        
+        if (drone.position.x === drone.initialGroundPos && drone.position.y < drone.altitude) {
+            drone.position.y += drone.velocity;
+            // ascend 
+        }
+        else if (drone.position.x >= drone.initialGroundPos &&   
+            drone.position.x <= drone.destination &&
+            drone.position.y === drone.altitude) {
+            if (drone.destination - drone.position.x < 1000)
+                drone.position.x = drone.destination;
+            else
+                drone.position.x += drone.velocity;
+            // mid flight
+        }
+        else if (drone.position.x === drone.destination && drone.position.y > 0) {
+            drone.position.y -= drone.velocity;
+            // descending
+        } else if (drone.position.x === drone.destination && drone.position.y === 0) {
+            drone.handleLanding();
+            // landing
+        }
+
+        if (drone.droneCollides()) {
+            this.position = positionBackup;
+        }
     }
 }
 
@@ -83,7 +125,6 @@ class Drone {
         if (!this.state.onGround) {
             this.nextStateInAir();
             this.state.timeUntilLanding = this.calcTimeUntilLanding();
-
         } else {
             this.nextStateOnGround();
         }
@@ -104,7 +145,7 @@ class Drone {
 
         const altitudeManager = new AltitudeManager(this.controlSystem);
         if (altitudeManager.requestAltitude(this)) {
-            console.log(`Drone number ${this.id} asked for a permit to take off and received a permit + ${this.altitude}.`);
+            console.log(`Drone number ${this.id} asked for a permit to take off and received a permit + ${this.altitude.value}.`);
             this.handleTakeOff();
         } else {
             console.log(`Drone number ${this.id} asked for a permit and didn't receive it`);
@@ -117,37 +158,17 @@ class Drone {
 
 
     nextStateInAir() {
-        let positionBackup = { x: this.position.x, y: this.position.y };
-        if (this.position.x === this.destination && this.position.y !== 0) {
-            this.position.y -= this.velocity;
-            // descend
-        }
-        else if (this.position.y !== this.altitude && this.position.x === this.initialGroundPos) {
-            this.position.y += this.velocity;
-            // ascend 
-        }
-        else if (this.position.y === 0 && this.position.x === this.destination) {
-            // landed
-            this.handleLanding();
-        } else {
-            if (this.destination - this.position.x < 1000)
-                this.position.x = this.destination;
-            else
-                this.position.x += this.velocity;
-            // move towards the destination
-        }
-
-        if (this.droneCollides()) {
-            this.position = positionBackup;
-        }
+        let flightManager = new FlightManager(this.controlSystem);
+        flightManager.setNextFlightState(this);;
     }
 
     handleLanding() {
+         const altitudeManager = new AltitudeManager(this.controlSystem);
         console.log(`Drone number ${this.id} has landed`);
         this.state.onGround = true;
         this.hasPassenger = false;
         this.initialGroundPos = this.position.x;
-        this.altitude = undefined;
+        altitudeManager.removeAltitude(this);
         this.state.timeUntilSomeoneReaches = this.controlSystem.getTimeUntilSomeoneReaches();
         this.state.timeUntilLanding = 0;
         this.destination = this.position.x + this.controlSystem.getDestination();
@@ -155,8 +176,8 @@ class Drone {
     }
 
     droneCollides() {
-        let restDrones = this.controlSystem.drones.filter(d => d.id !== this.id);
-        return restDrones.some(d => d.position.x === this.position.x && d.position.y === this.position.y);
+        let otherDrones = this.controlSystem.drones.filter(d => d.id !== this.id);
+        return otherDrones.some(d => d.position.x === this.position.x && d.position.y === this.position.y);
     }
 
     // returns time in minutes
@@ -194,15 +215,14 @@ class ControlSystem {
     drones = [];
 
     constructor(droneCount, altitudesCount) {
-        this.initializeAltitudes(altitudesCount);
         this.initializeDrones(droneCount);
+        this.initializeAltitudes(altitudesCount);
         this.assignDronesInitialGroundPositions();
-        //this.assignDronesAltitudes();
     }
 
     initializeAltitudes(altitudesCount) {
         for (let i = 1; i <= altitudesCount; i++) {
-            this.altitudes.push(1000 * i);
+            this.altitudes.push({ value: 1000 * i, drone: undefined });
         }
     }
 
@@ -235,12 +255,6 @@ class ControlSystem {
         }
     }
 
-    assignDronesAltitudes() {
-        for (let i = 0; i < this.altitudes.length; i++) {
-            this.drones[i].altitude = this.altitudes[i];
-        }
-    }
-
     getTimeUntilSomeoneReaches() {
         return randomNext(1, 16);
     }
@@ -254,10 +268,11 @@ class Simulation {
     start() {
         const controlSystem = new ControlSystem(20, 15);
 
-        for (let ticker = 1; ticker <= 240; ticker++) {
+        for (let ticker = 1; ticker <= 2400; ticker++) {
             controlSystem.calculateDroneStates();
             console.log(`minute #${ticker}`);
         }
+        
     }
 }
 

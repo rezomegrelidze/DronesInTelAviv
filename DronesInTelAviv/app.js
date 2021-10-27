df56254df87ccfac5e2f2a173dd885a600798914
Raz Megrelidze <rezomegrelidze@gmail.com>
2021-10-27 23:07:38 +0300

    added delay functionality

--- DronesInTelAviv/app.js
+++ DronesInTelAviv/app.js
@@ -1,16 +1,15 @@
 const ControlSystem = require('./controlSystem');
 
 class Simulation {
-    constructor() {
-
-    }
     
-    start() {
+    async start() {
         const controlSystem = new ControlSystem(20, 15);
 
         for (let ticker = 1; ticker <= 240; ticker++) {
+            const delay = ms => new Promise(resolve => setTimeout(resolve,ms));
             controlSystem.calculateDroneStates();
             console.log(`minute #${ticker}`);
+            await delay(500);
         }
         
     }
